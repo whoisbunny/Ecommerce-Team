d9ef175a7bfce5a865b78283aa0589fe500bf0b5
Bunny <kamleshrathod2092000@gmail.com>
2023-08-05 00:16:28 +0530

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import '../styles/header.scss'
+import img1 from '../assets/images/apneck.png'
 const Header = () => {
   return (
     <header>
       <div className="logo_section">
-        <img src="../assets/images/apneck.png" alt="" />
+        <img src={img1} alt="apneck" />
       </div>
       <nav className="nav_section">more content</nav>
     </header>
