9a01ccb6ba35f67b83771fc82e12b7f629f54044
Bunny <kamleshrathod2092000@gmail.com>
2023-08-05 16:15:02 +0530

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,32 @@
 import '../styles/header.scss'
 import img1 from '../assets/images/apneck.png'
+import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
+
       <div className="logo_section">
         <img src={img1} alt="apneck" />
       </div>
-      <nav className="nav_section">more content</nav>
+
+      <div className="nav_section">
+          <div className="search_box">
+            <input type="text"  className='search_input' placeholder='Find Products ...'  />
+            <button className="search_btn">Search</button>
+          </div>
+
+          <nav className="nav_links">
+            <Link> Home </Link>
+            <Link> Home </Link>
+            <Link> Home </Link>
+          </nav>
+
+        <nav className="side_links">
+          <Link>Cart</Link>
+          <Link>Cart</Link>
+        </nav>
+
+      </div>
     </header>
   );
 }
